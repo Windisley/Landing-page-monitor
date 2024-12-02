057a91f7d9581a2eb4592d4b04270395fc1ea6bf
Windisley <windisleyl@gmail.com>
2024-12-02 13:26:55 -0300

    Configurando o Typescript, Tailwind css e Pastas importantes

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", 
+    "./*.{html,js}"],        
+  theme: {
+    extend: {
+      colors:{
+        primaryblue: " #60c5e0",
+        secundaryblue: "#42BADA",
+        terciaryblue: " #2394B3",
+        white: "#E3F5FA",
+        dark: "#0C313C",
+      },
+      maxWidth:{
+        contain: "1500px",
+      },
+      margin:{
+        centralize: "0 auto",
+      },
+      translate:{
+        oneeixo: "-50%",
+        alleixo: "-50%, -50%",
+      },
+      screens:{
+        smallmoba: "300px",
+      },
+      fontSize:{
+        title: "2rem",
+        subtitle: "1rem",
+        paragraph: ".8rem",
+      },
+
+
+
+    },
+  },
+  plugins: [],
+};
