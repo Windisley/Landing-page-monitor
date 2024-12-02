52e18a80117f59d0b6252dc25a6d563ef14c3b77
Windisley <windisleyl@gmail.com>
2024-12-02 17:30:53 -0300

    Logica do carrossel pronta

--- build/script.js
+++ build/script.js
@@ -1,3 +1,19 @@
 "use strict";
 const carrossel = document.querySelector("[data-carrossel]");
-console.log(carrossel);
+function infiniteCarousel() {
+    let count = 0;
+    setInterval(() => {
+        carrossel.style.transition = "transform 0.5s ease-in-out";
+        carrossel.style.transform = `translateX(${(count * -100)}%)`;
+        count++;
+        if (count >= carrossel.children.length - 1) {
+            setTimeout(() => {
+                carrossel.style.transition = "none";
+                carrossel.style.transform = `translateX(0)`;
+                count = 0;
+                carrossel.appendChild(carrossel.children[0]);
+            }, 500);
+        }
+    }, 1500);
+}
+infiniteCarousel();
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,3 +1,25 @@
 const carrossel = document.querySelector("[data-carrossel]") as HTMLDivElement
 
-console.log(carrossel)
+function infiniteCarousel():void{
+    let count = 0;
+    setInterval(()=>{
+        carrossel.style.transition = "transform 0.5s ease-in-out";
+        carrossel.style.transform = `translateX(${( count * -100)}%)`
+
+        count++
+          
+        if(count >= carrossel.children.length -1){
+            setTimeout(()=>{
+                carrossel.style.transition = "none";
+                carrossel.style.transform = `translateX(0)`;
+                count = 0;
+
+                carrossel.appendChild(carrossel.children[0])
+
+            }, 500)
+        }
+    }, 1500)
+    
+}
+
+infiniteCarousel()
